071c65426f5061f9999f0279bca08acaad82a575
LGUG2Z <LGUG2Z@fastmail.com>
2024-10-13 20:38:21 -0700

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/27e30d177e57d912d614c88c622dcfdb2e6e6515' (2024-10-01)
      → 'github:nixos/nixpkgs/5633bcff0c6162b9e4b5f1264264611e950c8ec7' (2024-10-09)
    • Updated input 'nixvim':
        'github:nix-community/nixvim/0ca98d02104f7f0a703787a7a080a570b7f1bedd' (2024-10-02)
      → 'github:nix-community/nixvim/619e24366e8ad34230d65a323d26ca981bfa6927' (2024-10-13)
    • Updated input 'nixvim/devshell':
        'github:numtide/devshell/67cce7359e4cd3c45296fb4aaf6a19e2a9c757ae' (2024-07-27)
      → 'github:numtide/devshell/dd6b80932022cea34a019e2bb32f6fa9e494dfef' (2024-10-07)
    • Updated input 'nixvim/git-hooks':
        'github:cachix/git-hooks.nix/2f5ae3fc91db865eff2c5a418da85a0fbe6238a3' (2024-10-01)
      → 'github:cachix/git-hooks.nix/eb74e0be24a11a1531b5b8659535580554d30b28' (2024-10-12)
    • Updated input 'nixvim/home-manager':
        'github:nix-community/home-manager/ffe2d07e771580a005e675108212597e5b367d2d' (2024-09-26)
      → 'github:nix-community/home-manager/d57112db877f07387ce7104b5ac346ede556d2d7' (2024-10-12)
    • Updated input 'nixvim/nix-darwin':
        'github:lnl7/nix-darwin/f61d5f2051a387a15817007220e9fb3bbead57b3' (2024-09-30)
      → 'github:lnl7/nix-darwin/48b50b3b137be5cfb9f4d006835ce7c3fe558ccc' (2024-10-08)
    • Updated input 'nixvim/nixpkgs':
        'github:NixOS/nixpkgs/06cf0e1da4208d3766d898b7fdab6513366d45b9' (2024-09-29)
      → 'github:NixOS/nixpkgs/5633bcff0c6162b9e4b5f1264264611e950c8ec7' (2024-10-09)
    • Updated input 'nixvim/nuschtosSearch':
        'github:NuschtOS/search/c3c3928b8de7d300c34e9d90fdc19febd1a32062' (2024-09-29)
      → 'github:NuschtOS/search/9578d865b081c29ae98131caf7d2f69a42f0ca6e' (2024-10-12)
    • Updated input 'nixvim/treefmt-nix':
        'github:numtide/treefmt-nix/879b29ae9a0378904fbbefe0dadaed43c8905754' (2024-09-27)
      → 'github:numtide/treefmt-nix/4446c7a6fc0775df028c5a3f6727945ba8400e64' (2024-10-03)

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -7,17 +7,17 @@
       gopls.enable = pkgs.lib.mkDefault true;
       jsonls.enable = pkgs.lib.mkDefault true;
       marksman.enable = pkgs.lib.mkDefault true;
-      nil-ls.enable = pkgs.lib.mkDefault true;
+      nil_ls.enable = pkgs.lib.mkDefault true;
       pyright.enable = pkgs.lib.mkDefault true;
-      ts-ls.enable = pkgs.lib.mkDefault true;
-      lua-ls.enable = pkgs.lib.mkDefault true;
-      rust-analyzer = {
+      ts_ls.enable = pkgs.lib.mkDefault true;
+      lua_ls.enable = pkgs.lib.mkDefault true;
+      rust_analyzer = {
         enable = pkgs.lib.mkDefault true;
         installCargo = pkgs.lib.mkDefault false;
         installRustc = pkgs.lib.mkDefault false;
       };
       tailwindcss.enable = pkgs.lib.mkDefault true;
-      typst-lsp.enable = pkgs.lib.mkDefault true;
+      typst_lsp.enable = pkgs.lib.mkDefault true;
       cssls.enable = pkgs.lib.mkDefault true;
       html.enable = pkgs.lib.mkDefault true;
       htmx.enable = pkgs.lib.mkDefault true;
